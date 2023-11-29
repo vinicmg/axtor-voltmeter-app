2de1d9423b7b9cc0f8f53cf41d985b450329be89
vinicius.camargo <viniciuscamargo61@gmail.com>
2023-11-29 11:32:04 -0300

    Step by Step referência

--- src/views/stepbystep/PlateView.vue
+++ src/views/stepbystep/PlateView.vue
@@ -1,16 +1,26 @@
 <template>
-    <v-stepper v-model="e1">
+    <v-stepper v-model="e1" alt-labels>
         <template v-slot:default="{ prev, next }">
             <v-stepper-header>
-                <template v-for="n in steps" :key="`${n}-step`">
-                    <v-stepper-item :complete="e1 > n" :step="`Step {{ n }}`" :value="n"></v-stepper-item>
+                <template v-for="(n, i) in steps" :key="`${n}`">
+                    <v-stepper-item :complete="e1 > n" :value="n" :title="this.stepConfig[i]"></v-stepper-item>
                     <v-divider v-if="n !== steps" :key="n"></v-divider>
                 </template>
             </v-stepper-header>
 
-            <v-stepper-window class="d-flex justify-content-center">
+            <v-stepper-window>
                 <v-stepper-window-item :value="1">
-                    <v-card class="d-flex justify-center" color="grey-lighten-1" height="200" width="500px">Etapa 1</v-card>
+                    <v-card border height="65vh" class="d-flex justify-center pa-5">
+                        <v-sheet border elevation="4" width="35vw">
+                            <v-img :gradient="`to top right, rgba(255, 255, 255, .1), rgba(246, 147, 30, .4)`"
+                                :src="testImg" height="200"></v-img>
+                            <div class="d-flex justify-center mt-2">
+                                <h3>
+                                    Conferência Mecânica
+                                </h3>
+                            </div>
+                        </v-sheet>
+                    </v-card>
                 </v-stepper-window-item>
             </v-stepper-window>
 
@@ -22,13 +32,28 @@
 <script>
 import PlateService from '@/services/PlateService'
 import { defineComponent } from 'vue'
+import icon from '@/assets/icon.png'
 
 export default defineComponent({
     data: () => ({
         e1: 1,
-        steps: 15,
+        steps: 11,
+        stepConfig: [
+            'Conferência Mecânica',
+            'Cabos/Chicote Elétrico',
+            'Acessórios Elétricos',
+            'Carcaça vazia',
+            'Trilhos',
+            'Conferência de Gaveta',
+            'Montagem de Display(Tela)',
+            'Adesivagem',
+            'Placas',
+            'Qualidade',
+            'Embalagem'
+        ],
         currentPlateId: null,
         plate: null,
+        testImg: icon,
     }),
     created() {
         this.initialize()
